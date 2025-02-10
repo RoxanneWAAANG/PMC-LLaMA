a42918831b3fd806de3b689494af3454480e7d28
Ruoxin WANG <ruoxinwang@Ruoxin-MBP.lan>
2025-02-10 11:17:55 -0500

    upload

--- llama.py
+++ llama.py
@@ -4,3 +4,4 @@ model_path = snapshot_download(repo_id="chaoyi-wu/PMC_LLAMA_7B")
 print(f"Model downloaded to: {model_path}")
 # Model downloaded to:
 # /Users/ruoxinwang/.cache/huggingface/hub/models--chaoyi-wu--PMC_LLAMA_7B/snapshots/6caf5c19bdcd157f9d9a7d374be66d7b61d75351
+# C:\Users\admin\.cache\huggingface\hub\models--chaoyi-wu--PMC_LLAMA_7B\snapshots\6caf5c19bdcd157f9d9a7d374be66d7b61d75351
